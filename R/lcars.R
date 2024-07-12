1ce03137230f78265d57397f03f1a9069c4e3a22
Nelson Stauffer <ngstauffer@gmail.com>
2024-07-12 12:51:29 -0700

    Add sound options to lcars_trycatch()

--- R/lcars.R
+++ R/lcars.R
@@ -29,18 +29,26 @@ lcars <- function(sound = "attention") {
 #' Evaluate an expressions, playing appropriate audio samples of LCARS sounds from Star Trek
 #' @description
 #' Evaluates and returns the output from an expression. If there expression produces an error or a warning, an appropriate sound effect from Star Trek The Next Generation will be played.
-#' @param expr An expression to evaluate
+#' @param expr An expression to evaluate.
+#' @param warning Character string. The name of the sound to play if a warning is produced by the expression. Defaults to \code{"attention"}.
+#' @param errorCharacter string. The name of the sound to play if an error is produced by the expression. Defaults to \code{"error"}.
 #' @examples
 #' \dontrun{
 #' # This will produce an error message and sound effect
 #' lcars_trycatch(expr = "Kirk" / "Spock")
+#'
+#' # This will produce an error message and use a more dramatic sound effect
+#' lcars_trycatch(expr = "Kirk" / "Spock",
+#'                error = "alert")
 #'
 #' # This will produce an output, a warning message, and a sound effect
 #' lcars_trycatch(expr = as.numeric(c("7", "of", "9")))
 #' }
 #'
 #' @export
-lcars_trycatch <- function(expr) {
+lcars_trycatch <- function(expr,
+                           warning = "attention",
+                           error = "error") {
   output <- tryCatch(expr = withCallingHandlers(expr = expr,
                                                 warning = function(warning) {
                                                   lcars("warning")

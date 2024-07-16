59f484bc14bc6e818aba6d004094e06105149758
Nelson Stauffer <ngstauffer@gmail.com>
2024-07-16 12:28:41 -0700

    Add success handling to lcars_trycatch()

--- R/lcars.R
+++ R/lcars.R
@@ -30,10 +30,17 @@ lcars <- function(sound = "attention") {
 #' @description
 #' Evaluates and returns the output from an expression. If there expression produces an error or a warning, an appropriate sound effect from Star Trek The Next Generation will be played.
 #' @param expr An expression to evaluate.
-#' @param warning Character string. The name of the sound to play if a warning is produced by the expression. Valid options are: \code{"alert"}, \code{"alert_long"}, \code{"attention"}, \code{"attention_alt"}, \code{"error"}, and \code{"warning"}. Defaults to \code{"attention"}.
+#' @param success  Character string. The name of the sound to play if the expression executes without warnings or errors. Valid options are: \code{"alert"}, \code{"alert_long"}, \code{"attention"}, \code{"attention_alt"}, \code{"error"}, and \code{"warning"}. Defaults to \code{"attention"}.
+#' @param warning Character string. The name of the sound to play if a warning is produced by the expression. Valid options are: \code{"alert"}, \code{"alert_long"}, \code{"attention"}, \code{"attention_alt"}, \code{"error"}, and \code{"warning"}. Defaults to \code{"warning"}.
 #' @param error Character string. The name of the sound to play if an error is produced by the expression. Valid options are: \code{"alert"}, \code{"alert_long"}, \code{"attention"}, \code{"attention_alt"}, \code{"error"}, and \code{"warning"}. Defaults to \code{"error"}.
 #' @examples
 #' \dontrun{
+#' # This will produce the result without warnings/errors and play a sound effect
+#' lcars::lcars_trycatch(expr = c("NCC-1701",
+#'                                paste("NCC-1701",
+#'                                      c("A", "B", "C", "D", "E", "F", "G", "J"),
+#'                                      sep = "-")))
+#'
 #' # This will produce an error message and sound effect
 #' lcars_trycatch(expr = "Kirk" / "Spock")
 #'
@@ -47,15 +54,25 @@ lcars <- function(sound = "attention") {
 #'
 #' @export
 lcars_trycatch <- function(expr,
-                           warning = "attention",
+                           success = "attention",
+                           warning = "warning",
                            error = "error") {
+  successful <- TRUE
   output <- tryCatch(expr = withCallingHandlers(expr = expr,
                                                 warning = function(warning_returned) {
                                                   lcars(warning)
+                                                  # This bit lets us prevent the
+                                                  # success sound effect from
+                                                  # playing if we got a warning
+                                                  parent_environment <- parent.env(environment())
+                                                  parent_environment$successful <- FALSE
                                                 },
                                                 error = function(error_returned) {
                                                   lcars(error)
                                                   stop(error_returned)
                                                 }))
+  if (successful) {
+    lcars(success)
+  }
   return(output)
 }
